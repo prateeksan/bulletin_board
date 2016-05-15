af39af664a8f852dc61601ff8d41e43b87d123af
prateeksan <sanyal.prateek@gmail.com>
2016-05-15 11:28:59 -0400

    notes made draggable, app connected to Bacon Ipsum API, readme added

--- js/Note.js
+++ js/Note.js
@@ -12,6 +12,9 @@ var Note = React.createClass({
             transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)' 
         };
     },
+    componentDidMount: function() {
+        $(this.getDOMNode()).draggable();
+    },
     edit: function() {
         this.setState({editing: true});
     },
@@ -71,6 +74,16 @@ var Board = React.createClass({
         this.uniqueId = this.uniqueId || 0;
         return this.uniqueId++;
     },
+    componentWillMount: function() {
+        var self = this;
+        if (this.props.count) {
+            $.getJSON("http://baconipsum.com/api/?type=app-meat&sentences=" + this.props.count + "&start-with=lorem=1&callback=?", function(results){
+                results[0].split('. ').forEach(function(sentence){
+                    self.add(sentence.substring(0, 40));
+                });
+            });
+        }
+    },
     add: function(text) {
         var arr = this.state.notes;
         arr.push({
@@ -104,5 +117,5 @@ var Board = React.createClass({
     }
 })
 
-React.render(<Board count={10}/>, 
+React.render(<Board count={20}/>, 
     document.getElementById('react-container'));
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Bulletin Board
+A simple bulletin board built using ReactJS. (This project was built for educational/experimental purposes only).
+## Getting Started
++ '$ python -m SimpleHTTPServer 8000'
++ Visit 'localhost:8000' in your browser.
+## Features
++ Stateless bulletin board rendered using react components.
++ Receives default notes from the Bacon Ipsum API.
++ Add, update and delete notes.
++ Drag notes around the screen.
